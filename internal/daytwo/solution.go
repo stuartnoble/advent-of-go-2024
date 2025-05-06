36a3859806d9304c3053f545fbd633a899e443fd
Stuart Noble <github@stuartnoble.com>
2025-05-07 00:43:56 +0100

    Adding notes for another solution

--- internal/daytwo/solution.go
+++ internal/daytwo/solution.go
@@ -10,6 +10,16 @@ func compareLevels(first, second int) (isIncrease, isDecrease bool, distance int
 	return first < second, first > second, utils.Abs(first - second)
 }
 
+// Possible recursive method: pass in previous distance, first, second
+// Initial call: 0, 7, 6 -> safe
+// 2nd call: -1, 6, 4 -> safe
+// 3rd call: -2, 4, 2 -> safe
+
+// Initial call: 0, 1, 2 -> safe
+// 2nd call: 1, 2, 7 -> unsafe (+5)
+
+// Initial call: 0, 1, 3 -> safe
+// 2nd call: 2, 3, 2 -> unsafe (+2 then -1)
 func isSafeReport(report [5]int) bool {
 	isAllSafeLevels := true
 	isIncreasing := false
