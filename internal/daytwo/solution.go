ab8033a856e7a206b05c6bd87780e73ea9ee4381
Stuart Noble <github@stuartnoble.com>
2025-05-10 00:11:00 +0100

    Finally got the right answer in a not entirely hideous way

--- internal/daytwo/solution.go
+++ internal/daytwo/solution.go
@@ -16,7 +16,7 @@ var fileSystem embed.FS
 
 const maxLevelDelta int = 3
 
-func checkIsSafeDelta(levels []int, maxDelta int) (bool, int) {
+func checkIsSafeDelta(levels []int, maxDelta int) bool {
 	firstDistance := levels[1] - levels[0]
 	secondDistance := levels[2] - levels[1]
 
@@ -26,42 +26,36 @@ func checkIsSafeDelta(levels []int, maxDelta int) (bool, int) {
 	isWithinLimit := utils.Abs(firstDistance) <= maxDelta &&
 		utils.Abs(secondDistance) <= maxDelta
 
-	// If firstDistance is greater than secondDistance, put the second number in a variable, otherwise put the third number in a variable
-
 	return isTrending && isWithinLimit
 }
 
-func isSafeReport(report []int) (bool, bool) {
+func isSafeReport(report []int) bool {
 	// Start by assuming the report is safe
 	var isSafeReport = true
-	///var isSafeWhenUnmodified bool = false
-	//var isSafeWhenModified bool = false
-	var isModified bool = false
 
-	for i := 0; i < len(report)-2; {
-		isSafeReport = checkIsSafeDelta(report[i:i+3], maxLevelDelta)
-
-		i++
-
-		if !isSafeReport && !isModified {
-			// Don't do it this way, it will change the original slice!
-			// modifiedSlice := append(report[:i+1], report[i+2:]...)
-
-			// Don't forget to include index you want (+1)
-			modifiedReport := append([]int{}, report[:i]...)
-			modifiedReport = append(modifiedReport, report[i+1:]...)
-			report = modifiedReport
-
-			isModified = true
-
-			// Recheck the same index
-			i--
-
-			//isSafeWhenModified = isSafeWhenModified && checkIsSafeDelta(modifiedReport[i:i+3], maxLevelDelta)
-		}
+	for i := 0; i < len(report)-2; i++ {
+		isSafeReport = isSafeReport && checkIsSafeDelta(report[i:i+3], maxLevelDelta)
 	}
 
-	return isSafeReport && !isModified, isSafeReport && isModified
+	return isSafeReport
+}
+
+func isSafeReportWhenModified(report []int, indexToRemove int) bool {
+	if indexToRemove == len(report) {
+		return false
+	}
+
+	modifiedReport := append([]int{}, report[:indexToRemove]...)
+
+	if indexToRemove < len(report)-1 {
+		modifiedReport = append(modifiedReport, report[indexToRemove+1:]...)
+	}
+
+	if !isSafeReport(modifiedReport) {
+		return isSafeReportWhenModified(report, indexToRemove+1)
+	}
+
+	return true
 }
 
 func getSafeReportCount(p *DayTwoPuzzle) (int, int) {
@@ -69,13 +63,14 @@ func getSafeReportCount(p *DayTwoPuzzle) (int, int) {
 	safeModifiedReportsCount := 0
 
 	for i := range p.reports {
-		isSafeWhenUnmodified, isSafeWhenModified := isSafeReport(p.re	ports[i])
+		fmt.Println("Checking report", i)
+		isSafeReportResult := isSafeReport(p.reports[i])
 
-		if isSafeWhenUnmodified {
+		if isSafeReportResult {
 			safeUnmodifiedReportsCount++
 		}
 
-		if isSafeWhenModified {
+		if !isSafeReportResult && isSafeReportWhenModified(p.reports[i], 0) {
 			safeModifiedReportsCount++
 		}
 	}
